47f8c1d3343ca303bcc9783fe1ee7eb0f76a5c8e
Aiwe <maksymivv@gmail.com>
2019-01-17 01:12:43 +0200

    Changed config for Karbo

--- src/config.ts
+++ src/config.ts
@@ -1,30 +1,30 @@
 let global : any = typeof window !== 'undefined' ? window : self;
 global.config = {
-	apiUrl:typeof window !== 'undefined' && window.location ? window.location.href.substr(0,window.location.href.lastIndexOf('/')+1)+'api/' : 'https://www.plenteum.com/api/',
-	mainnetExplorerUrl: "http://block-explorer.plenteum.com/",
-	testnetExplorerUrl: "http://testnet.msrchain.net/",
+	apiUrl:typeof window !== 'undefined' && window.location ? window.location.href.substr(0,window.location.href.lastIndexOf('/')+1)+'api/' : 'https://www.wallet.karbo.org/api/',
+	mainnetExplorerUrl: "http://explorer.karbo.org/",
+	testnetExplorerUrl: "http://explorer.karbo.org/",
 	testnet: false,
-    coinUnitPlaces: 8,
+    coinUnitPlaces: 12,
     coinDisplayUnitPlaces: 2,
-	txMinConfirms: 20,         
-	txCoinbaseMinConfirms: 20, 
-	addressPrefix: 18181,
-	integratedAddressPrefix: 29,
-	addressPrefixTestnet: 33,
-	integratedAddressPrefixTestnet: 34,
-	subAddressPrefix: 52,
-	subAddressPrefixTestnet: 73,
-	feePerKB: new JSBigInt('1000000'), //for testnet its not used, as fee is dynamic.
-	dustThreshold: new JSBigInt('1000000'),//used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
+	txMinConfirms: 10,         
+	txCoinbaseMinConfirms: 10,
+	addressPrefix: 111,
+	integratedAddressPrefix: 112,
+	addressPrefixTestnet: 111,
+	integratedAddressPrefixTestnet: 112,
+	subAddressPrefix: 113,
+	subAddressPrefixTestnet: 113,
+	feePerKB: new JSBigInt('1000000000000'), //for testnet its not used, as fee is dynamic.
+	dustThreshold: new JSBigInt('100000000'),//used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
 	defaultMixin: 0, // default value mixin
 
 	idleTimeout: 30,
 	idleWarningDuration: 20,
 
-	coinSymbol: 'PLE',
-	openAliasPrefix: "ple",
-	coinName: 'Plenteum',
-	coinUriPrefix: 'plenteum:',
-	avgBlockTime: 120,
+	coinSymbol: 'KRB',
+	openAliasPrefix: "krb",
+	coinName: 'Karbo',
+	coinUriPrefix: 'karbowanec:',
+	avgBlockTime: 240,
 	maxBlockNumber: 500000000,
 };
