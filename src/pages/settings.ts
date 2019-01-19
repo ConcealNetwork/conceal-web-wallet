8c695368449900c35e6f1b6d959e0918c968bec3
Aiwe <maksymivv@gmail.com>
2019-01-19 22:58:13 +0200

    Correct delete wallet modal strings

--- src/pages/settings.ts
+++ src/pages/settings.ts
@@ -81,8 +81,8 @@ class SendView extends DestructableView{
 			title: i18n.t('settingsPage.deleteWalletModal.title'),
 			html: i18n.t('settingsPage.deleteWalletModal.content'),
 			showCancelButton: true,
-			confirmButtonText: i18n.t('global.deleteWalletModal.confirmText'),
-			cancelButtonText: i18n.t('global.deleteWalletModal.cancelText'),
+			confirmButtonText: i18n.t('settingsPage.deleteWalletModal.confirmText'),
+			cancelButtonText: i18n.t('settingsPage.deleteWalletModal.cancelText'),
 		}).then((result:any) => {
 			if (result.value) {
 				AppState.disconnect();
