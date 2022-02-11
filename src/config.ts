34ab4d965a5ee04c042c0bd147b632cea4c8884f
Øx <35565671+krypt0x@users.noreply.github.com>
2022-02-11 12:45:59 +0000

    subAddressPrefixTestnet
    subAddressPrefixTestnet

--- src/config.ts
+++ src/config.ts
@@ -11,20 +11,24 @@ global.config = {
 	mainnetExplorerUrl: "https://explorer.conceal.network/",
 	mainnetExplorerUrlHash: "https://explorer.conceal.network/index.html?hash={ID}#blockchain_transaction",
 	mainnetExplorerUrlBlock: "https://explorer.conceal.network/index.html?hash={ID}#blockchain_block",
+	
 	testnetExplorerUrl: "http://explorer.testnet.conceal.network/",
 	testnetExplorerUrlHash: "https://explorer.testnet.conceal.network/index.html?hash={ID}#blockchain_transaction",
 	testnetExplorerUrlBlock: "https://explorer.testnet.conceal.network/index.html?hash={ID}#blockchain_block",
 	testnet: false,
-    coinUnitPlaces: 6,
+    
+	coinUnitPlaces: 6,
     coinDisplayUnitPlaces: 6,
 	txMinConfirms: 10,
 	txCoinbaseMinConfirms: 10,
+	
 	addressPrefix: 31444,
 	integratedAddressPrefix: 31445,
+	subAddressPrefix: 31446,
 	addressPrefixTestnet: 31444,
 	integratedAddressPrefixTestnet: 31445,
-	subAddressPrefix: 31446,
-	subAddressPrefixTestnet: 31447,
+	subAddressPrefixTestnet: 31446,
+	
 	coinFee: new JSBigInt('1000'),
 	feePerKB: new JSBigInt('1000'), //for testnet its not used, as fee is dynamic.
 	dustThreshold: new JSBigInt('10'),//used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
@@ -38,6 +42,7 @@ global.config = {
 	openAliasPrefix: "ccx",
 	coinName: 'Conceal',
 	coinUriPrefix: 'conceal:',
+	
 	avgBlockTime: 120,
 	maxBlockNumber: 500000000,
 };
