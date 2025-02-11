b9de0300cad04052a69f2ebb51ffca028e79ddd9
Acktarius <acktarius@protonmail.com>
2025-02-11 17:29:47 -0500

    copyright update + whitelist update

--- src/lib/numbersLab/messageClick.ts
+++ src/lib/numbersLab/messageClick.ts
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2024 Acktarius
- * Copyright (c) 2018-2024 Conceal Community, Conceal.Network & Conceal Devs
+ * Copyright (c) 2018-2025 Conceal Community, Conceal.Network & Conceal Devs
 */
 
 /**
